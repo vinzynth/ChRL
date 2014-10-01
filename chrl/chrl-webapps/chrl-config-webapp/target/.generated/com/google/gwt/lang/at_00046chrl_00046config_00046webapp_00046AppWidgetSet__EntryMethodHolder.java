91e108c3d0c9b43de76f30a987bc2e87f032baea
vinzynth <vinzynthnihilit@gmail.com>
2014-10-01 20:42:31 +0200

    Cleanup
    Maven cleanup

--- chrl/chrl-webapps/chrl-config-webapp/target/.generated/com/google/gwt/lang/at_00046chrl_00046config_00046webapp_00046AppWidgetSet__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class at_00046chrl_00046config_00046webapp_00046AppWidgetSet__EntryMethodHolder {
-  public static final void init() {
-    // Filled in by the compiler to call entry methods.
-  }
-}
