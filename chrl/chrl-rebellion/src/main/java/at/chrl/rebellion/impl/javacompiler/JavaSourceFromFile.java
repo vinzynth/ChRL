86c2fad713a2677c3cfb242d6db6312f9f8cfe2b
Christian <leopold.christian@gmx.at>
2014-10-04 23:06:50 +0300

    ChRL-Rebellion added ChRL-Database Services added

---
+++ chrl/chrl-rebellion/src/main/java/at/chrl/rebellion/impl/javacompiler/JavaSourceFromFile.java
@@ -0,0 +1,58 @@
+/**
+ * This file is part of aion-lightning <aion-lightning.org>.
+ * 
+ * aion-lightning is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * aion-lightning is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with aion-lightning.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.chrl.rebellion.impl.javacompiler;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.tools.SimpleJavaFileObject;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * This class is simple wrapper for SimpleJavaFileObject that load class source from file sytem
+ * 
+ * @author SoulKeeper
+ */
+public class JavaSourceFromFile extends SimpleJavaFileObject {
+
+	/**
+	 * Construct a JavaFileObject of the given kind and with the given File.
+	 * 
+	 * @param file
+	 *          the file with source of this file object
+	 * @param kind
+	 *          the kind of this file object
+	 */
+	public JavaSourceFromFile(File file, Kind kind) {
+		super(file.toURI(), kind);
+	}
+
+	/**
+	 * Returns class source represented as string.
+	 * 
+	 * @param ignoreEncodingErrors
+	 *          not used
+	 * @return class source
+	 * @throws IOException
+	 *           if something goes wrong
+	 */
+	@Override
+	public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
+		return FileUtils.readFileToString(new File(this.toUri()));
+	}
+}
---
+++ chrl/chrl-rebellion/src/main/java/at/chrl/rebellion/metadata/OnClassUnload.java
@@ -0,0 +1,37 @@
+/**
+ * This file is part of aion-lightning <aion-lightning.org>.
+ * 
+ * aion-lightning is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * aion-lightning is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with aion-lightning.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.chrl.rebellion.metadata;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Method marked as {@link OnClassUnload} will be called when there is a script reload or shutdown.<br>
+ * Only static methods with no arguments can be marked with this annotation.<br>
+ * This is only used if {@link at.chrl.rebellion.ScriptContext#getClassListener()} returns instance of
+ * {@link at.chrl.rebellion.classlistener.OnClassLoadUnloadListener} subclass.
+ * 
+ * @author SoulKeeper
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OnClassUnload {
+}
