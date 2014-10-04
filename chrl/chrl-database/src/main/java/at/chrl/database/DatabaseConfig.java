eb786cbf760ea54e453265a815f653fdb23a63b0
Christian <leopold.christian@gmx.at>
2014-10-04 23:44:31 +0300

    Changed some Dependencies

--- chrl/chrl-database/src/main/java/at/chrl/database/DatabaseConfig.java
+++ chrl/chrl-database/src/main/java/at/chrl/database/DatabaseConfig.java
@@ -18,6 +18,7 @@ package at.chrl.database;
 
 import java.io.File;
 
+import at.chrl.nutils.configuration.ConfigUtil;
 import at.chrl.nutils.configuration.Property;
 
 /**
@@ -26,53 +27,55 @@ import at.chrl.nutils.configuration.Property;
  * @author SoulKeeper
  */
 public class DatabaseConfig {
+	
+	static { ConfigUtil.loadAndExport(DatabaseConfig.class); }
 
 	/**
 	 * Default database url.
 	 */
-	@Property(key = "database.url", defaultValue = "jdbc:mysql://localhost:3306/aion_uni")
+	@Property(key = "chrl.database.url", defaultValue = "jdbc:mysql://localhost:3306/aion_uni")
 	public static String DATABASE_URL;
 
 	/**
 	 * Name of database Driver
 	 */
-	@Property(key = "database.driver", defaultValue = "com.mysql.jdbc.Driver")
+	@Property(key = "chrl.database.driver", defaultValue = "com.mysql.jdbc.Driver")
 	public static Class<?> DATABASE_DRIVER;
 
 	/**
 	 * Default database user
 	 */
-	@Property(key = "database.user", defaultValue = "root")
+	@Property(key = "chrl.database.user", defaultValue = "root")
 	public static String DATABASE_USER;
 
 	/**
 	 * Default database password
 	 */
-	@Property(key = "database.password", defaultValue = "root")
+	@Property(key = "chrl.database.password", defaultValue = "root")
 	public static String DATABASE_PASSWORD;
 
     /**
      * Amount of partitions used by BoneCP
      */
-    @Property(key = "database.bonecp.partition.count", defaultValue = "2")
+    @Property(key = "chrl.database.bonecp.partition.count", defaultValue = "2")
     public static int DATABASE_BONECP_PARTITION_COUNT;
 
 	/**
 	 * Minimum amount of connections that are always active in bonecp partition
 	 */
-	@Property(key = "database.bonecp.partition.connections.min", defaultValue = "2")
+	@Property(key = "chrl.database.bonecp.partition.connections.min", defaultValue = "2")
 	public static int DATABASE_BONECP_PARTITION_CONNECTIONS_MIN;
 
 	/**
 	 * Maximum amount of connections that are allowed to use in bonecp partition
 	 */
-	@Property(key = "database.bonecp.partition.connections.max", defaultValue = "5")
+	@Property(key = "chrl.database.bonecp.partition.connections.max", defaultValue = "5")
 	public static int DATABASE_BONECP_PARTITION_CONNECTIONS_MAX;
 
 	/**
 	 * Location of database script context descriptor
 	 */
-	@Property(key = "database.scriptcontext.descriptor", defaultValue = "./data/scripts/system/database/database.xml")
+	@Property(key = "chrl.database.scriptcontext.descriptor", defaultValue = "./data/scripts/system/database/database.xml")
 	public static File DATABASE_SCRIPTCONTEXT_DESCRIPTOR;
 	
 }
