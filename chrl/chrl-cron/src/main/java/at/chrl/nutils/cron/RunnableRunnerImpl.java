8dd93cc1150a477c896cb7f5f42ee8c4d6bd24dd
vinzynth <vinzynthnihilit@gmail.com>
2015-02-24 14:33:34 +0100

    added default implementation

---
+++ chrl/chrl-cron/src/main/java/at/chrl/nutils/cron/RunnableRunnerImpl.java
@@ -0,0 +1,17 @@
+/**
+ * @author leopoldc
+ * Feb 24, 2015 - 2:29:26 PM
+ * chrl-cron
+ * at.chrl.nutils.cron
+ */
+package at.chrl.nutils.cron;
+
+/**
+ * 
+ * @author leopoldc
+ * Default Implementation
+ *
+ */
+public class RunnableRunnerImpl implements RunnableRunner{
+
+}
