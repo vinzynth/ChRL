bc843805ee4ad70c3f302ec0a658b2cb22eb6faa
vinzynth <vinzynthnihilit@gmail.com>
2015-02-26 23:04:48 +0100

    Config Webapp removed

--- chrl/chrl-webapps/chrl-config-webapp/src/main/webapp/VAADIN/widgetsets/at.chrl.config.webapp.AppWidgetSet/deferredjs/7EDB232E6F9351EF1E2674438C1ECD98/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.at_chrl_config_webapp_AppWidgetSet.runAsyncCallback1('i8(1148,1,xnd);_.nc=function i$b(){CPb((!xPb&&(xPb=new DPb),xPb),this.b.e)};Ebd(Vh)(1);\n//# sourceURL=at.chrl.config.webapp.AppWidgetSet-1.js\n')
