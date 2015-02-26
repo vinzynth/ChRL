49dec10caddf9b51baa5fc60538c8251883c4da5
vinzynth <vinzynthnihilit@gmail.com>
2015-02-26 23:59:31 +0100

    Added chrl spring sample archtype

---
+++ chrl-archetypes/chrl-spring-sample/src/main/resources/archetype-resources/src/main/java/SampleController.java
@@ -0,0 +1,17 @@
+package ${package};
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ChRL Spring Archtype
+ * 
+ */
+@RestController
+public class SampleController {
+
+	@RequestMapping("sample")
+	public String helloSpring(){
+		return "Hello Spring";
+	}
+}
