24b12fe3d6ed0e2f219e204f0fadfcc2af3aca90
vinzynth <vinzynthnihilit@gmail.com>
2015-03-13 21:14:04 +0100

    StreamUtils added

---
+++ chrl/chrl-utils/src/main/java/at/chrl/nutils/StreamUtils.java
@@ -0,0 +1,44 @@
+/**
+ * @author Christian Richard Leopold - ChRL
+ * Mar 13, 2015 - 9:01:13 PM
+ * chrl-utils
+ * at.chrl.nutils
+ */
+package at.chrl.nutils;
+
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * @author Christian Richard Leopold - ChRL <br>
+ * Mar 13, 2015 - 9:01:13 PM
+ *
+ */
+public final class StreamUtils {
+
+	private StreamUtils() {}
+	
+	public static <T> Function<T, T> identity(){
+		return t -> t;
+	}
+	
+	public static <T> BinaryOperator<T> keepFirst(){
+		return (t, t2) -> t;
+	}
+	
+	public static <T> BinaryOperator<T> keepLast(){
+		return (t1, t) -> t;
+	}
+	
+	public static <T, K> Collector<T, ?, Map<K, T>> toMap(Function<T, K> keyMapper){
+		return Collectors.toMap(keyMapper, StreamUtils.identity(), StreamUtils.keepFirst());
+	}
+	
+	public static <T, K> Collector<T, ?, Map<K, T>> toMapKeepLast(Function<T, K> keyMapper){
+		return Collectors.toMap(keyMapper, StreamUtils.identity(), StreamUtils.keepLast());
+	}
+	
+}
