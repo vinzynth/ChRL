7216485a413bab8f5f9b858b2cc14f714e3d1d36
vinzynth <vinzynthnihilit@gmail.com>
2015-07-08 13:47:20 +0200

    Added getKeyComparator and getValueComparator to StreamUtils

--- chrl/chrl-utils/src/main/java/at/chrl/nutils/StreamUtils.java
+++ chrl/chrl-utils/src/main/java/at/chrl/nutils/StreamUtils.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
@@ -26,32 +27,76 @@ public final class StreamUtils {
 
 	private StreamUtils() {}
 	
+	/**
+	 * identity function
+	 * @return identity function
+	 */
 	public static <T> Function<T, T> identity(){
 		return t -> t;
 	}
 	
+	/**
+	 * to null function
+	 * @return to null function
+	 */
+	public static <T> Function<T, T> toNull(){
+		return t -> null;
+	}
+	
+	/**
+	 * binary keep first function
+	 * @return keep first function
+	 */
 	public static <T> BinaryOperator<T> keepFirst(){
 		return (t, t2) -> t;
 	}
 	
+	/**
+	 * binary keep last function
+	 * @return keep last function
+	 */
 	public static <T> BinaryOperator<T> keepLast(){
 		return (t1, t) -> t;
 	}
 	
+	/**
+	 * {@link Collectors#toMap(Function, Function, BinaryOperator, java.util.function.Supplier)} override
+	 * returns toMap function with given key Mapper.
+	 * Keeps first if duplicates
+	 *  
+	 * @param keyMapper
+	 * @return mapper function
+	 */
 	public static <T, K> Collector<T, ?, Map<K, T>> toMap(Function<T, K> keyMapper){
 		return Collectors.toMap(keyMapper, StreamUtils.identity(), StreamUtils.keepFirst());
 	}
 	
+	/**
+	 * {@link Collectors#toMap(Function, Function, BinaryOperator, java.util.function.Supplier)} override
+	 * returns toMap function with given key Mapper.
+	 * Keeps last if duplicates
+	 *  
+	 * @param keyMapper
+	 * @return mapper function
+	 */
 	public static <T, K> Collector<T, ?, Map<K, T>> toMapKeepLast(Function<T, K> keyMapper){
 		return Collectors.toMap(keyMapper, StreamUtils.identity(), StreamUtils.keepLast());
 	}
 	
-	public static final <T, R extends Comparable<R>> Comparator<T> getComparator(Function<T, R> getter, boolean reverse){
-		if(reverse)
-			return (e1, e2) -> getter.apply(e2).compareTo(getter.apply(e1));
-		return (e1, e2) -> getter.apply(e1).compareTo(getter.apply(e2));
-	}
-	
+	/**
+	 *  creates a range stream of given type T.
+	 *  <p>
+	 *  checks if value after applying the increment function equals the end value.
+	 *  <p>
+	 *  <b>
+	 *  BEWARE: INFITE LOOP IF END VALUE IS NEVER REACHED BY INCREMENT FUNCTION
+	 *  </b>
+	 *  
+	 * @param start - start value of stream
+	 * @param end - end value of stream
+	 * @param incrementFunction - increment function
+	 * @return stream
+	 */
 	public static final <T> Stream<T> rangeStream(T start, T end, Function<T, T> incrementFunction){
 		List<T> range = new ArrayList<T>();
 		range.add(start);
@@ -63,11 +108,35 @@ public final class StreamUtils {
 		return range.stream();
 	}
 	
+	/**
+	 * returns comparator for gicen getter function
+	 * 
+	 * @param getter
+	 * @param reverse for reverse rodering
+	 * @return
+	 */
+	public static final <T, R extends Comparable<R>> Comparator<T> getComparator(Function<T, R> getter, boolean reverse){
+		if(reverse)
+			return (e1, e2) -> getter.apply(e2).compareTo(getter.apply(e1));
+		return (e1, e2) -> getter.apply(e1).compareTo(getter.apply(e2));
+	}
+	
+	/**
+	 * returns comparator by getter functions
+	 * @param getters
+	 * @return comparator
+	 */
 	@SafeVarargs
 	public static final <T, R extends Comparable<R>> Comparator<T> getComparator(Function<T, R>... getters){
 		return getComparator(false, getters);
 	}
-	
+
+	/**
+	 * returns comparator by getter functions
+	 * @param reverse - for reverse ordering
+	 * @param getters
+	 * @return comparator
+	 */
 	@SafeVarargs
 	public static final <T, R extends Comparable<R>> Comparator<T> getComparator(boolean reverse, Function<T, R>... getters){
 		return (e1, e2) -> {
@@ -81,6 +150,69 @@ public final class StreamUtils {
 		};
 	}
 	
+	/**
+	 * Returns Comparator for quick map entry sorting
+	 * Compares by key
+	 * 
+	 * @param reverse - for reverse sorting
+	 * 
+	 * @type E - Entry
+	 * @type K - Entry Key
+	 * @type V - Entry Value
+	 * 
+	 * @return key comparator
+	 */
+	public static final <E extends Entry<K,V>, K extends Comparable<K>, V> Comparator<E> getKeyComparator(boolean reverse){
+		if(reverse)
+			return (e1, e2) -> e2.getKey().compareTo(e1.getKey());
+		return (e1, e2) -> e1.getKey().compareTo(e2.getKey());
+	}
+	
+	/**
+	 * Returns Comparator for quick map entry sorting
+	 * Compares by key
+	 * 
+	 * @type E - Entry
+	 * @type K - Entry Key
+	 * @type V - Entry Value
+	 * 
+	 * @return key comparator
+	 */
+	public static final <E extends Entry<K,V>, K extends Comparable<K>, V> Comparator<E> getKeyComparator(){
+		return getKeyComparator(false);
+	}
+	
+	/**
+	 * Returns Comparator for quick map entry sorting
+	 * Compares by value
+	 * 
+	 * @param reverse - for reverse sorting
+	 * 
+	 * @type E - Entry
+	 * @type K - Entry Key
+	 * @type V - Entry Value
+	 * 
+	 * @return value comparator
+	 */
+	public static final <E extends Entry<K,V>, K, V extends Comparable<V>> Comparator<E> getValueComparator(boolean reverse){
+		if(reverse)
+			return (e1, e2) -> e2.getValue().compareTo(e1.getValue());
+		return (e1, e2) -> e1.getValue().compareTo(e2.getValue());
+	}
+	
+	/**
+	 * Returns Comparator for quick map entry sorting
+	 * Compares by value
+	 * 
+	 * @type E - Entry
+	 * @type K - Entry Key
+	 * @type V - Entry Value
+	 * 
+	 * @return value comparator
+	 */
+	public static final <E extends Entry<K,V>, K, V extends Comparable<V>> Comparator<E> getValueComparator(){
+		return getValueComparator(false);
+	}
 	
 	public static final <R, T> Function<R, T> parse(final Function<R, T> parseFunction, final T defaultValue){
 		return s -> {
