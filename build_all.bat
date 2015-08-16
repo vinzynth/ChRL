167e727341565d7114b9933f7df1d56f4652d50d
vinzynth <vinzynthnihilit@gmail.com>
2015-08-16 22:46:37 +0200

    Added gpg sign goal to maven build script

--- build_all.bat
+++ build_all.bat
@@ -1,4 +1,4 @@
-mvn clean install -DskipTests -T3C -f chrl-bom\pom.xml
+mvn clean install gpg:sign -DskipTests -T3C -f chrl-bom\pom.xml
 
-mvn clean install -DskipTests -T3C -f chrl\pom.xml
-mvn clean install -f chrl-archetypes\chrl-spring-sample\pom.xml
+mvn clean install gpg:sign -DskipTests -T3C -f chrl\pom.xml
+mvn clean install gpg:sign -f chrl-archetypes\chrl-spring-sample\pom.xml
