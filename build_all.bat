d7dd5e7e8e6630c43a8e6b98775edeb792531c0c
vinzynth <vinzynthnihilit@gmail.com>
2015-08-17 02:09:59 +0200

    Renamed chrl Folder to chrl-parent

--- build_all.bat
+++ build_all.bat
@@ -1,4 +1,1 @@
-mvn clean install gpg:sign -DskipTests -T3C -f chrl-bom\pom.xml
-
-mvn clean install gpg:sign -DskipTests -T3C -f chrl\pom.xml
-mvn clean install gpg:sign -f chrl-archetypes\chrl-spring-sample\pom.xml
+mvn clean install gpg:sign -DskipTests -T3C
