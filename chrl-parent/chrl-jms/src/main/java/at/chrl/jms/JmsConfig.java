5e9eb97a8f70f9ead63504c3fded4b9d8a6fc423
vinzynth <vinzynthnihilit@gmail.com>
2015-08-24 21:56:36 +0200

    Insertet Spring JMS guide

---
+++ chrl-parent/chrl-jms/src/main/java/at/chrl/jms/JmsConfig.java
@@ -0,0 +1,43 @@
+/**
+ * This file is part of ChRL Util Collection.
+ * 
+ * ChRL Util Collection is free software: you can redistribute it and/or
+ * modify  it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * ChRL Util Collection is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ChRL Util Collection.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.chrl.jms;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+
+/**
+ * @author Vinzynth
+ * 24.08.2015 - 21:50:54
+ *
+ */
+@EnableJms
+@Configuration
+public class JmsConfig {
+
+	@Bean
+	JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
+		SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		return factory;
+	}
+}
