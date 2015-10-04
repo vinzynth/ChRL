3d7854cdfe64db0794cd7e7e9468adfd1985cb52
vinzynth <vinzynthnihilit@gmail.com>
2015-10-05 00:27:48 +0200

    Added Config Server

---
+++ chrl-parent/chrl-config-server/src/main/java/at/chrl/config/ConfigServerApp.java
@@ -0,0 +1,40 @@
+/**
+ * This file is part of ChRL Util Collection.
+ * 
+ * ChRL Util Collection is free software: you can redistribute it and/or
+ * modify  it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * ChRL Util Collection is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ChRL Util Collection.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.chrl.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Vinzynth
+ * 04.10.2015 - 21:44:10
+ *
+ */
+@Configuration
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+public class ConfigServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApp.class, args);
+	}
+}
