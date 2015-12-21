e6fcfede305ca717ba1fad68cbf272d0a9fe8053
Christian Leopold <christian.leopold@bravestone.com>
2015-12-21 14:35:57 +0100

    Added Module Dependencies

---
+++ chrl-parent/chrl-config-spring-integration/src/main/java/at/chrl/config/spring/SpringConfigUtil.java
@@ -0,0 +1,22 @@
+package at.chrl.config.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Spring Config Util
+ *
+ * Provodes ApplicationConfigContext
+ *
+ * Created by vinzynth on 21.12.15.
+ */
+@Configuration
+public class SpringConfigUtil {
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+}
